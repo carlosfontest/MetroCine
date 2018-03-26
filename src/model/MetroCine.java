3b3eb43222742fd72d347f473ed1863b092b6c77
Carlos Fontes <fontessystems@gmail.com>
2018-03-26 16:27:11 -0400

    Minor Changes

--- src/model/MetroCine.java
+++ src/model/MetroCine.java
@@ -5,7 +5,7 @@ import controller.Controlador;
  * MetroCine. Software de ventas.
  * Estructura de Datos 1718-2
  * Universidad Metropolitana (Caracas, Venezuela)
- * @author Carlos Fontes & Rafael Quintero
+ * @author Carlos Fontes & Rafael Quintero.
  */
 public class MetroCine {
 
